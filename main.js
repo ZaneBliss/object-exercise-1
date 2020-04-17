8ae295d6cfdaa06d615c242244e2e0e55459edb7
zanebliss <zanebliss@icloud.com>
2020-04-17 10:52:13 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let car = {
+    year: 2015,
+    make: "Ford",
+    model: "Mustang",
+}
+let animals = ['Kippers', 'Jack', 'Gypsy', 'Angus', 'Seymour Bouts', 'Sharkey']
+let dad = {
+    name: 'Derek',
+    age: 42
+}
+let mom = {
+    name: 'Katherine',
+    age: 38
+}
+let son = {
+    name: 'George',
+    age: 15
+}
+let daughter = {
+    name: 'Melody',
+    age: 13
+}
+let familyMembers = [dad, mom, son, daughter];
